5658e558c2b356d0cd4ba63eb332f5675fc84aa7
Johan Brook <johan.jvb@gmail.com>
2014-03-20 00:51:32 +0100

    Initial commit

---
+++ test/tests.js
@@ -0,0 +1,29 @@
+var should = chai.should(),
+		Logger = window.RymdLogger
+
+describe("Logger", function() {
+
+	var logger = new Logger("TestModule")
+			logger2 = new Logger("TestModule2")
+
+	it("should have different verbosity levels", function() {
+		Logger.Levels.LEVEL_GLOBAL.should.exist
+		Logger.Levels.LEVEL_DEBUG.should.exist
+		Logger.Levels.LEVEL_ALL.should.exist
+	})
+
+	it("should log global level", function() {
+		logger.global("Test")
+		logger.debug("Test")
+
+		logger.separator();
+
+		logger2.debug("Test 2")
+
+		logger.separator();
+
+		Logger.view("TestModule2")
+		logger.debug("Test")
+		logger2.debug("Test 2")
+	})
+})
